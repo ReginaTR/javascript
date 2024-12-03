0038cad2ca0c7d226dfd5b06c06a7484d14bd01e
ReginaTR <regininhar@hotmail.com>
2024-12-03 17:37:51 -0300

    Boolean1 Values

---
+++ boolean.js
@@ -0,0 +1,11 @@
+let isSad = true;
+let isHappy = !isSad;
+let isFeeling = isSad || isHappy;
+let isConfusing = isSad && isHappy;
+
+
+console.log(isSad);
+console.log(isHappy);
+console.log(isHappy && isSad);
+console.log(isFeeling);
+console.log(isConfusing);
